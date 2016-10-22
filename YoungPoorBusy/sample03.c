551bb5400b10a0a103d7c8d5940c66e90b1477cd
Moritama <moritama1515@gmail.com>
2016-10-23 02:59:23 +0900

    Add a sample03

---
+++ YoungPoorBusy/sample03.c
@@ -0,0 +1,29 @@
+//データ数の行と列車データの行を判別する
+#include<stdio.h>
+
+int main()
+{
+  int count;
+  char buf[64];
+
+  while(1){
+    /*データセットの先頭（データ数）を読む*/
+    if(fgets(buf,sizeof(buf),stdin) == NULL){
+      break;
+    }
+
+    sscanf(buf,"%d",&count);
+    printf("%d\n",count);
+    
+    if(count == 0){ /*データ全体の終了の判定*/
+      break;
+    }
+
+    /*データセット（列車データ）を読み込む*/
+    while(count-->0){
+      if(fgets(buf,sizeof(buf),stdin)==NULL){
+        break;
+      }
+    }
+  }
+}

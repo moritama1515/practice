9dfc54e19c97cc90e7361fc5357c54ab8cb0b7c1
Moritama <moritama1515@gmail.com>
2016-11-04 23:40:27 +0900

    Create a sample04

--- YoungPoorBusy/sample04.c
+++ YoungPoorBusy/sample04.c
@@ -1,5 +1,20 @@
-//データセットを意識して読み込む
+//列車データの処理
 #include<stdio.h>
+#include<string.h>
+
+#define MAXCITY 100
+
+char city_name[MAXCITY][18];
+int ncity;
+int nconn;
+
+#define MAXCONN 2000+1
+
+struct train{
+  int from,to;
+  int dpt,arv;
+  int fare;
+}trains[MAXCONN];
 
 int main()
 {
@@ -27,3 +42,39 @@ int main()
     }
   }
 }
+
+parse_connection(char *buf)
+{
+  char from[18],to[18];
+  int dpt[2],arv[2],fare;
+
+  sscanf(buf,"%s %d:%d %s %d:%d %d",
+      from,&(dpt[0]),&(dpt[1]),to,&(arv[0]),&(arv[1]),&fare);
+
+  trains[nconn].from = city_id(from);
+  trains[nconn].to = city_id(to);
+  trains[nconn].dpt = dpt[0]*60+dpt[1];
+  trains[nconn].arv = arv[0]*60+arv[1];
+  trains[nconn].fare = fare;
+}
+
+int city_id(char *name)
+{
+  int i;
+
+  for(i = 0;i < ncity; i++){
+    if(strcmp(name,&(city_name[i][0])) == 0){
+      return i;
+    }
+  }
+
+  strcpy(&(city_name[ncity][0]),name);
+  return ncity++;
+}
+
+int diff_time(int start_time,int end_time){
+  int ans;
+  ans = end_time - start_time;
+  return ans;
+}
+
